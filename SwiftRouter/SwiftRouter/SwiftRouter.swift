dbcf6d4d123d02aac08c8c203cbe772d0eadb3a0
Ramy Kfoury <ramy.kfoury@zalando.de>
2015-05-07 11:50:11 +0200

    made routes a dictionary of <route,closure>

--- SwiftRouter/SwiftRouter/SwiftRouter.swift
+++ SwiftRouter/SwiftRouter/SwiftRouter.swift
@@ -6,18 +6,22 @@ import Foundation
 
 public class Router {
     
+    public typealias RouteClosure = () -> ()
+    
+    static var emptyClosure: RouteClosure = {}
+    
     public init() {}
     
-    lazy var routes: [String] = {
-        return [String]()
+    lazy var routes: [String: RouteClosure] = {
+        return [String: RouteClosure]()
     }()
     
     public func numberOfRoutes() -> Int {
         return count(routes)
     }
     
-    public func addRoute(route: String) -> Self {
-        routes.append(route)
+    public func addRoute(route: String, closure: RouteClosure? = emptyClosure) -> Self {
+        routes[route] = closure
         return self
     }
 }
